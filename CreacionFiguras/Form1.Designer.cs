bb7617f91580075566d1d1d697d8fbc3a6d8b66b
Andrea Evelyn Mejía Rubio <andiemartin33@gmail.com>
2021-03-09 09:50:55 -0600

    Agregar archivos de proyecto.

---
+++ CreacionFiguras/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CreacionFiguras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lstfiguras = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(30, 55);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(359, 296);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // lstfiguras
+            // 
+            this.lstfiguras.FormattingEnabled = true;
+            this.lstfiguras.Items.AddRange(new object[] {
+            "Circulo",
+            "Rectangulo"});
+            this.lstfiguras.Location = new System.Drawing.Point(430, 55);
+            this.lstfiguras.Name = "lstfiguras";
+            this.lstfiguras.Size = new System.Drawing.Size(120, 95);
+            this.lstfiguras.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 450);
+            this.Controls.Add(this.lstfiguras);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox lstfiguras;
+    }
+}
+
---
+++ CreacionFiguras/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreacionFiguras
+{
+    public partial class Form1 : Form
+    {
+        //variables que determinan la ubicacion del click
+        int x, y;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point punto = new Point(e.X,e.Y);
+            x = punto.X;
+            y = punto.Y;
+
+            panel1.Invalidate();
+
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics papel = panel1.CreateGraphics();
+            Pen lapiz = new Pen(Color.Black);
+
+            if (lstfiguras.SelectedIndex==0)
+            {
+                SolidBrush brocha = new SolidBrush(Color.Red);
+                papel.DrawEllipse(lapiz, x - 50, y - 50, 100, 100);
+                papel.FillEllipse(brocha, x - 50, y - 50, 100, 100);
+            }
+
+            if (lstfiguras.SelectedIndex == 1)
+            {
+                SolidBrush brocha = new SolidBrush(Color.Green);
+                papel.DrawRectangle(lapiz, x - 50, y - 50, 100, 100);
+                papel.FillRectangle(brocha, x - 50, y - 50, 100, 100);
+            }
+
+
+
+        }
+    }
+}
